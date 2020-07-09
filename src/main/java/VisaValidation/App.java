16e21e2a97d6f3d0dd95d85ea3bc2ee2fa5c885f
sbasu <shilpa.basu@difrent.co.uk>
2020-07-09 11:52:53 +0100

    Added pom.xml file with initial config

---
+++ src/main/java/VisaValidation/App.java
@@ -0,0 +1,13 @@
+package VisaValidation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
